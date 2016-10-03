f33e05042c89a65c7d1e22c4f014e82bf5d8faeb
ghj0504520 <ghj0504520.cywjjy@gmail.com>
2016-10-03 20:22:39 +0800

    ghj0504520(2016/10/03)

--- physic_web/physic_web.js
+++ physic_web/physic_web.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	var videoArt="https://www.youtube.com/embed/BCkHnvDGWOY?enablejsapi=1";
 	
 	/*改變engineering的網址*/
-	var videoEng="https://www.youtube.com/embed/II1g92jFItE";
+	var videoEng="https://www.youtube.com/embed/II1g92jFItE?enablejsapi=1";
 	
 	/*改變technology的網址*/
 	var videoTec="https://www.youtube.com/embed/BCkHnvDGWOY?enablejsapi=1";
@@ -30,97 +30,110 @@ $(document).ready(function() {
 	$('#art_btn').click();
 	$('#iframeArt').attr('src', videoArt+"&autoplay=1");
 
+	if(videoArt.length) {
+		$("#collapseArt1").on("show.bs.collapse", function () {
 
-	$("#collapseArt1").on("show.bs.collapse", function(){
+			$('#iframeArt')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+		});
 
-		$('#iframeArt')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-	});
+		$("#collapseArt1").on("hide.bs.collapse", function () {
+			$('#iframeArt')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+		});
 
-	$("#collapseArt1").on("hide.bs.collapse", function(){
-		$('#iframeArt')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-	});
-
-	$("#modalArt").on('show.bs.modal', function () {
-		$('#iframeArt').attr('src', videoArt+"&autoplay=1");
-	});
-	$("#modalArt").on('hide.bs.modal', function () {
-		$('#iframeArt').attr('src', videoArt+"&autoplay=0");
-	});
-
-
-
-
-	$("#collapseEng1").on("show.bs.collapse", function(){
-
-		$('#iframeEng')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-	});
-
-	$("#collapseEng1").on("hide.bs.collapse", function(){
-		$('#iframeEng')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-	});
-
-
-	$("#modalEngineering").on('show.bs.modal', function () {
-		$('#iframeEng').attr('src', videoEng+"&autoplay=1");
-	});
-	$("#modalEngineering").on('hide.bs.modal', function () {
-		$('#iframeEng').attr('src', videoEng+"&autoplay=0");
-	});
-
-
-	$("#collapseTec1").on("show.bs.collapse", function(){
-
-		$('#iframeTec')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-	});
-
-	$("#collapseTec1").on("hide.bs.collapse", function(){
-		$('#iframeTec')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-	});
-
-	$("#modalTechnology").on('show.bs.modal', function () {
-		$('#iframeTec').attr('src', videoTec+"&autoplay=1");
-	});
-	$("#modalTechnology").on('hide.bs.modal', function () {
-		$('#iframeTec').attr('src', videoTec+"&autoplay=0");
-	});
+		$("#modalArt").on('show.bs.modal', function () {
+			$('#iframeArt').attr('src', videoArt + "&autoplay=1");
+		});
+		$("#modalArt").on('hide.bs.modal', function () {
+			$('#iframeArt').attr('src', videoArt + "&autoplay=0");
+		});
+	}else{
+		$("#collapseArt1").remove();
+	}
 
 
 
+	if(videoEng.length) {
+		$("#collapseEng1").on("show.bs.collapse", function () {
 
-	$("#collapseSci1").on("show.bs.collapse", function(){
+			$('#iframeEng')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+		});
 
-		$('#iframeSci')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-	});
-
-	$("#collapseSci1").on("hide.bs.collapse", function(){
-		$('#iframeSci')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-	});
-
-	$("#modalScience").on('show.bs.modal', function () {
-		$('#iframeSci').attr('src', videoSci+"&autoplay=1");
-	});
-	$("#modalScience").on('hide.bs.modal', function () {
-		$('#iframeSci').attr('src', videoSci+"&autoplay=0");
-	});
+		$("#collapseEng1").on("hide.bs.collapse", function () {
+			$('#iframeEng')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+		});
 
 
+		$("#modalEngineering").on('show.bs.modal', function () {
+			$('#iframeEng').attr('src', videoEng + "&autoplay=1");
+		});
+		$("#modalEngineering").on('hide.bs.modal', function () {
+			$('#iframeEng').attr('src', videoEng + "&autoplay=0");
+		});
+	}else{
+		$("#collapseEng1").remove();
+	}
+
+	if(videoTec.length) {
+		$("#collapseTec1").on("show.bs.collapse", function () {
+
+			$('#iframeTec')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+		});
+
+		$("#collapseTec1").on("hide.bs.collapse", function () {
+			$('#iframeTec')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+		});
+
+		$("#modalTechnology").on('show.bs.modal', function () {
+			$('#iframeTec').attr('src', videoTec + "&autoplay=1");
+		});
+		$("#modalTechnology").on('hide.bs.modal', function () {
+			$('#iframeTec').attr('src', videoTec + "&autoplay=0");
+		});
+	}else{
+		$("#collapseTec1").remove();
+	}
 
 
-	$("#collapseMath1").on("show.bs.collapse", function(){
+	if(videoSci.length) {
+		$("#collapseSci1").on("show.bs.collapse", function () {
 
-		$('#iframeMath')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-	});
+			$('#iframeSci')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+		});
 
-	$("#collapseMath1").on("hide.bs.collapse", function(){
-		$('#iframeMath')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-	});
+		$("#collapseSci1").on("hide.bs.collapse", function () {
+			$('#iframeSci')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+		});
 
-	$("#modalMath").on('show.bs.modal', function () {
-		$('#iframeMath').attr('src', videoMath+"&autoplay=1");
-	});
-	$("#modalMath").on('hide.bs.modal', function () {
-		$('#iframeMath').attr('src', videoMath+"&autoplay=0");
-	});
-	
+		$("#modalScience").on('show.bs.modal', function () {
+			$('#iframeSci').attr('src', videoSci + "&autoplay=1");
+		});
+		$("#modalScience").on('hide.bs.modal', function () {
+			$('#iframeSci').attr('src', videoSci + "&autoplay=0");
+		});
+	}else{
+		$("#collapseSci1").remove();
+	}
+
+
+
+	if(videoMath.length) {
+		$("#collapseMath1").on("show.bs.collapse", function () {
+
+			$('#iframeMath')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+		});
+
+		$("#collapseMath1").on("hide.bs.collapse", function () {
+			$('#iframeMath')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+		});
+
+		$("#modalMath").on('show.bs.modal', function () {
+			$('#iframeMath').attr('src', videoMath + "&autoplay=1");
+		});
+		$("#modalMath").on('hide.bs.modal', function () {
+			$('#iframeMath').attr('src', videoMath + "&autoplay=0");
+		});
+	}else{
+		$("#collapseMath1").remove();
+	}
 	
 });
