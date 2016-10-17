cb5f3730b76c8300a4113277c42b2880690e7a5d
ghj0504520 <ghj0504520.cywjjy@gmail.com>
2016-10-17 15:55:25 +0800

    ghj0504520(2016/10/17)

--- physic_web/physic_web/physic_web.js
+++ physic_web/physic_web/physic_web.js
@@ -193,5 +193,34 @@ $(document).ready(function() {
 		}
 			
 	});
-	
+
+	elementMove();
 });
+
+var elementMove=function () {
+
+		// 取得其 top 值
+		var $allBTN= $('#allBTN'),
+			_top = $allBTN.offset().top;
+
+		// 當網頁捲軸捲動時
+		var $win = $(window).scroll(function(){
+			// 如果現在的 scrollTop 大於原本的 top 時
+			if($win.scrollTop() >= _top){
+				// 如果座標系統不是 fixed 的話
+				if($allBTN.css('position')!='fixed'){
+					// 設定座標系統為 fixed
+					$allBTN.css({
+						position: 'fixed',
+						top:10
+					});
+				}
+			}else{
+				// 還原座標系統為 absolute
+				$allBTN.css({
+					position: 'absolute',
+					top:570
+				});
+			}
+		});
+}
