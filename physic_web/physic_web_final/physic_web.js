a1cc1cc2f23f0b4f20022922269b8e5f9717eef1
robert80203 <robert80203@gmail.com>
2017-06-28 17:20:50 +0800

    Responsive web(moblie sidebar)

--- physic_web/physic_web_final/physic_web.js
+++ physic_web/physic_web_final/physic_web.js
@@ -308,3 +308,19 @@ $(document).ready(function() {
 	
 	//elementMove();
 });
+
+/*=================Responsive Desgin======================*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+	$('.sidebar').css('opacity', '0');
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+	$('.sidebar').css('opacity', '1');
+}
+
+/*=========================================================*/
+
